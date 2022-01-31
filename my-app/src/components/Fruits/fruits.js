945a599c156a6362492eecbdee7ff2d9da7d8348
Rodrigo Gastaud <rodrigogastaud_97@hotmail.com>
2022-01-31 00:54:35 -0300

    Cards com map, porém sem func de addto car

--- my-app/src/components/Fruits/fruits.js
+++ my-app/src/components/Fruits/fruits.js
@@ -1,21 +1,31 @@
-import Banana from "./Banana.jpg";
 import "./fruits.css";
 import { useState } from "react";
 
 const Fruits = (props) => {
-  const { children } = props;
+  const { children, image, price } = props;
   const [count, setCount] = useState(0);
 
   return (
     <div className="card">
       <p>{children}</p>
-      <img src={Banana} alt="Banana"></img>
-      <p>R$</p>
+      <img src={image} alt="fruit"></img>
+      <p>R$ {price}</p>
       <p>{count}</p>
-      <div className="card-button">
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <button onClick={() => count > 0 && setCount(count - 1)}>-</button>
+      <div className="card-quantity">
+        <button
+          className="card-quantity-button"
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+        <button
+          className="card-quantity-button"
+          onClick={() => count > 0 && setCount(count - 1)}
+        >
+          -
+        </button>
       </div>
+      <button className="card-submit">Adicionar ao carrinho</button>
     </div>
   );
 };
--- my-app/src/routes/CarShop.js
+++ my-app/src/routes/CarShop.js
@@ -1,9 +1,27 @@
 import Fruits from "../components/Fruits/fruits";
+import Banana from "../components/Fruits/Banana.jpg";
+import Maça from "../components/Fruits/Maça.jpg";
+import Laranja from "../components/Fruits/Laranja.jpg";
+import Abacate from "../components/Fruits/Abacate.jpg";
+import Manga from "../components/Fruits/Manga.jpg";
 
 const Home = () => {
+  const fruitList = [
+    { name: "Banana", unity: "kg", price: 6.0, image: Banana },
+    { name: "Maça", unity: "kg", price: 3.0, image: Maça },
+    { name: "Laranja", unity: "kg", price: 5.5, image: Laranja },
+    { name: "Abacate", unity: "unidade", price: 5.0, image: Abacate },
+    { name: "Manga", unity: "unidade", price: 6.0, image: Manga },
+  ];
+
   return (
     <div>
-      <Fruits>Banana</Fruits>
+      {fruitList.map((fruit) => (
+        <Fruits price={fruit.price} image={fruit.image}>
+          {fruit.name}
+        </Fruits>
+      ))}
+      {/* <Fruits image={Banana}>Banana</Fruits> */}
     </div>
   );
 };
