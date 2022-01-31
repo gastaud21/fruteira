c6e7d25572242200837dd174295923ac616bcdd7
Rodrigo Gastaud <rodrigogastaud_97@hotmail.com>
2022-01-30 22:08:48 -0300

    lógica do botão + e - pronta

--- my-app/src/components/Fruits/fruits.js
+++ my-app/src/components/Fruits/fruits.js
@@ -1,18 +1,20 @@
 import Banana from "./Banana.jpg";
 import "./fruits.css";
+import { useState } from "react";
 
 const Fruits = (props) => {
   const { children } = props;
+  const [count, setCount] = useState(0);
 
   return (
     <div className="card">
       <p>{children}</p>
       <img src={Banana} alt="Banana"></img>
       <p>R$</p>
-      <p>Unidades ou gramas</p>
+      <p>{count}</p>
       <div className="card-button">
-        <button>+</button>
-        <button>-</button>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => count > 0 && setCount(count - 1)}>-</button>
       </div>
     </div>
   );
